666bad50c6122922adb5e9f6bc669a41921e0811
George Yu <yudumpling@gmail.com>
2019-05-03 15:00:42 +0800

    Beautified front end files

--- libpass/static/scripts/student.js
+++ libpass/static/scripts/student.js
@@ -1,58 +1,57 @@
+$(document).ready(function() {
+    var state1 = 0;
 
-$(document).ready(function(){
-  var state1 = 0;
-
-  console.log(state1);
-  console.log(Cookies.get());
-  var stuid = Cookies.get("stuid");
-  document.getElementById("stuid").innerHTML = "Welcome<br><h3>"+stuid+"</h3>";
-
-  $.post("/get-state",{
-    id: stuid,
-  },
-  function(request){
-    if (request.code){
-      alert("An Unexpected Error has occured. Please Refresh the Page and Try Again");
-    }
-    else{
-      var sState = request.data;
-      switch (sState) {
-        case 0:
-          document.getElementById("state").innerHTML = "Not Signed Up";
-          break;
-        case 1:
-          document.getElementById("state").innerHTML = "Wait For Approval";
-          break;
-        case 2:
-          document.getElementById("state").innerHTML = "Request Approved";
-          break;
-        case 3:
-          document.getElementById("state").innerHTML = "In Library";
-          break;
-        case 4:
-          document.getElementById("state").innerHTML = "Going back to Dorm";
-          break;
-
-        default: document.getElementById("state").innerHTML = NULL;
-
-      }
-    }
-  });
-
-  $("#bookMe").click(function(){
-    state1 = 1;
-
-    $.post("/update-state",{
-      id: stuid,
-      state: state1,
-    },
-    function(data){
-    if (data.code){
-      alert("An Unexpected Error has occured. Please Refresh the Page and Try Again");
-    }else{
-      alert("You signed up");
-    }
-  });
     console.log(state1);
-  });
+    console.log(Cookies.get());
+    var stuid = Cookies.get("stuid");
+    document.getElementById("stuid").innerHTML = "Welcome<br><h3>" + stuid + "</h3>";
+
+    $.post("/get-state", {
+            id: stuid,
+        },
+        function(request) {
+            if (request.code) {
+                alert("An Unexpected Error has occured. Please Refresh the Page and Try Again");
+            } else {
+                var sState = request.data;
+                switch (sState) {
+                    case 0:
+                        document.getElementById("state").innerHTML = "Not Signed Up";
+                        break;
+                    case 1:
+                        document.getElementById("state").innerHTML = "Wait For Approval";
+                        break;
+                    case 2:
+                        document.getElementById("state").innerHTML = "Request Approved";
+                        break;
+                    case 3:
+                        document.getElementById("state").innerHTML = "In Library";
+                        break;
+                    case 4:
+                        document.getElementById("state").innerHTML = "Going back to Dorm";
+                        break;
+
+                    default:
+                        document.getElementById("state").innerHTML = NULL;
+
+                }
+            }
+        });
+
+    $("#bookMe").click(function() {
+        state1 = 1;
+
+        $.post("/update-state", {
+                id: stuid,
+                state: state1,
+            },
+            function(data) {
+                if (data.code) {
+                    alert("An Unexpected Error has occured. Please Refresh the Page and Try Again");
+                } else {
+                    alert("You signed up");
+                }
+            });
+        console.log(state1);
+    });
 });
